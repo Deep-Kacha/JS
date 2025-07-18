082940a0a71fd8b526a5a3c92d17beab20c2cc9f
Deep-Kacha <deepnkacha2256@gmail.com>
2025-07-18 11:48:10 +0530

    -

---
+++ 1.Basics/2_dataTypes.js
@@ -0,0 +1,20 @@
+//   alert (3+3) 
+console.log(3+3)
+
+let name ="DEEP"
+let age = 19
+let isLoggedIn = false
+let state = null
+
+//number =>  2 power 53
+// bigint  
+// String => in double code ""
+//bollean = True / False
+//null => standalone valuse (Empty value)
+// undefined = not declare
+//symbol = unique
+
+//object 
+console.log(typeof "Deep")
+console.log(typeof 18)
+console.log(typeof null)
